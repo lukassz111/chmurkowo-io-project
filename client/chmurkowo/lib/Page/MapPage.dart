a37b1bd078d5e7fa4f2a3df0702c8953c804d5ac
Łukasz Narloch <lukassz111@gmail.com>
2020-12-26 14:00:17 +0100

    empty MapPage

---
+++ client/chmurkowo/lib/Page/MapPage.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class MapPage extends StatefulWidget {
+  MapPage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MapPageStateState createState() => _MapPageStateState();
+}
+
+class _MapPageStateState extends State<MapPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body:
+          Center(), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
